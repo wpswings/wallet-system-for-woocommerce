ab01e2b77bcc7a5b5136ef89782935280561a033
shobhapatwal-cedcoss <shobhapatwal@makewebbetter.com>
2021-04-28 11:32:38 +0530

    added transaction_detail parameter in custom api for wallet

---
+++ .history/package/rest-api/class-wallet-system-for-woocommerce-rest-api_20210427113554.php
@@ -0,0 +1,290 @@
+<?php
+/**
+ * The file that defines the core plugin api class
+ *
+ * A class definition that includes api's endpoints and functions used across the plugin
+ *
+ * @link       https://makewebbetter.com/
+ * @since      1.0.0
+ *
+ * @package    Wallet_System_For_Woocommerce
+ * @subpackage Wallet_System_For_Woocommerce/package/rest-api/version1
+ */
+
+/**
+ * The core plugin  api class.
+ *
+ * This is used to define internationalization, api-specific hooks, and
+ * endpoints for plugin.
+ *
+ * Also maintains the unique identifier of this plugin as well as the current
+ * version of the plugin.
+ *
+ * @since      1.0.0
+ * @package    Wallet_System_For_Woocommerce
+ * @subpackage Wallet_System_For_Woocommerce/package/rest-api/version1
+ * @author     makewebbetter <webmaster@makewebbetter.com>
+ */
+class Wallet_System_For_Woocommerce_Rest_Api {
+
+	/**
+	 * The unique identifier of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      string    $plugin_name    The string used to uniquely identify this plugin.
+	 */
+	protected $plugin_name;
+
+	/**
+	 * The current version of the plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      string    $version    The current version of the plugin.
+	 */
+	protected $version;
+
+	/**
+	 * Define the core functionality of the plugin api.
+	 *
+	 * Set the plugin name and the plugin version that can be used throughout the plugin.
+	 * Load the dependencies, define the merthods, and set the hooks for the api and
+	 *
+	 * @since    1.0.0
+	 * @param   string $plugin_name    Name of the plugin.
+	 * @param   string $version        Version of the plugin.
+	 */
+	public function __construct( $plugin_name, $version ) {
+
+		$this->plugin_name = $plugin_name;
+		$this->version = $version;
+
+	}
+
+	/**
+     * Endpoint namespace.
+     *
+     * @var string
+     */
+    protected $namespace = 'wsfw-route/v1';
+
+    /**
+     * Route base.
+     *
+     * @var string
+     */
+    protected $base_url = '/wallet/';
+
+	/**
+	 * Define endpoints for the plugin.
+	 *
+	 * Uses the Wallet_System_For_Woocommerce_Rest_Api class in order to create the endpoint
+	 * with WordPress.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 */
+	public function mwb_wsfw_add_endpoint() {
+		
+		// for getting particular user wallet details
+		register_rest_route(
+			$this->namespace,
+			$this->base_url . '(?P<id>\d+)',
+			array(
+				array(
+					'args' => array(
+						'id' => array(
+							'description' => __( 'Unique user id of user.', 'wallet-system-for-woocommerce' ),
+							'type'        => 'integer',
+							'required'    => true,
+						),
+						'context' => array(
+							'default' => 'view',
+						),
+					),
+					'methods'  => WP_REST_Server::READABLE,
+					'callback' => array( $this, 'mwb_wsfw_user_wallet_balance' ),
+					'permission_callback' => array( $this, 'mwb_wsfw_get_permission_check' ),
+				),
+				// update wallet of user
+				array(
+					'args' => array(
+						'id' => array(
+							'description' => __( 'Unique user id of user.', 'wallet-system-for-woocommerce' ),
+							'type'        => 'integer',
+							'required'    => true,
+						),
+						'amount' => array(
+							'description' => __( 'Wallet transaction amount.', 'wallet-system-for-woocommerce' ),
+							'type'        => 'number',
+							'required'    => true,
+						),
+						'transaction_type' => array(
+							'type'        => 'string',
+							'description' => __( 'Wallet transaction type.', 'wallet-system-for-woocommerce' ),
+							'required'    => true,
+						),
+						'payment_method' => array(
+							'type'        => 'string',
+							'description' => __( 'Payment method used.', 'wallet-system-for-woocommerce' ),
+							'required'    => true,
+						),
+						'note' => array(
+							'description' => __( 'Note during wallet transfer.', 'wallet-system-for-woocommerce' ),
+							'type'        => 'string',
+						),
+						'order_id' => array(
+							'description' => __( 'If amount is deducted when wallet used as payment gateway.', 'wallet-system-for-woocommerce' ),
+							'type'        => 'number',
+						),
+					),
+					'methods'  => WP_REST_Server::EDITABLE,
+					'callback' => array( $this, 'mwb_wsfw_edit_wallet_balance' ),
+					'permission_callback' => array( $this, 'mwb_wsfw_update_item_permissions_check' ),
+				)
+				
+			)
+		);
+
+		// show transactions of particular user
+		register_rest_route(
+			$this->namespace,
+			$this->base_url . 'transactions/(?P<id>\d+)',
+			array(
+				'args' => array(
+					'id' => array(
+						'description' => __( 'Unique user id of user.', 'wallet-system-for-woocommerce' ),
+						'type'        => 'integer',
+						'required'    => true,
+					),
+					'context' => array(
+						'default' => 'view',
+					),
+				),
+				'methods'  => WP_REST_Server::READABLE,
+				'callback' => array( $this, 'mwb_wsfw_user_wallet_transactions' ),
+				'permission_callback' => array( $this, 'mwb_wsfw_get_permission_check' ),
+			)
+		);
+
+	}
+
+
+	/**
+	 * Begins validation process of api endpoint.
+	 *
+	 * @param   Array $request    All information related with the api request containing in this array.
+	 * @return  Array   $result   return rest response to server from where the endpoint hits.
+	 * @since    1.0.0
+	 */
+	public function mwb_wsfw_default_permission_check( $request ) {
+
+		// Add rest api validation for each request.
+		$result = true;
+		return $result;
+	}
+
+
+	/**
+	 * Begins execution of api endpoint.
+	 *
+	 * @param   Array $request    All information related with the api request containing in this array.
+	 * @return  Array   $mwb_wsfw_response   return rest response to server from where the endpoint hits.
+	 * @since    1.0.0
+	 */
+	public function mwb_wsfw_default_callback( $request ) {
+
+		require_once WALLET_SYSTEM_FOR_WOOCOMMERCE_DIR_PATH . 'package/rest-api/version1/class-wallet-system-for-woocommerce-api-process.php';
+		$mwb_wsfw_api_obj = new Wallet_System_For_Woocommerce_Api_Process();
+		$mwb_wsfw_resultsdata = $mwb_wsfw_api_obj->mwb_wsfw_default_process( $request );
+		if ( is_array( $mwb_wsfw_resultsdata ) && isset( $mwb_wsfw_resultsdata['status'] ) && 200 == $mwb_wsfw_resultsdata['status'] ) {
+			unset( $mwb_wsfw_resultsdata['status'] );
+			$mwb_wsfw_response = new WP_REST_Response( $mwb_wsfw_resultsdata, 200 );
+		} else {
+			$mwb_wsfw_response = new WP_Error( $mwb_wsfw_resultsdata );
+		}
+		return $mwb_wsfw_response;
+	}
+
+	/**
+	 * Begins validation process of api endpoint.
+	 *
+	 * @param   Array $request    All information related with the api request containing in this array.
+	 * @return  Array   $result   return rest response to server from where the endpoint hits.
+	 * @since    1.0.0
+	 */
+	public function mwb_wsfw_get_permission_check( $request ) {
+
+		// Add rest api validation for each request.
+		// if ( ! current_user_can( 'edit_something' )  ) {
+		// 	return new WP_Error( 'rest_forbidden', esc_html__( 'Sorry, you cannot list resources.', 'wallet-system-for-woocommerce' ), array( 'status' => 401 ) );
+		// }
+		return current_user_can( 'edit_posts' );
+		return is_user_logged_in();
+		//return true;
+	}
+
+	/**
+	 * Undocumented function
+	 *
+	 * @param [type] $request
+	 * @return void
+	 */
+	public function mwb_wsfw_update_item_permissions_check( $request ) {
+
+		// if ( ! current_user_can( 'edit_something' )  ) {
+		// 	return new WP_Error( 'rest_forbidden', esc_html__( 'Sorry, you cannot list resources.', 'wallet-system-for-woocommerce' ), array( 'status' => 401 ) );
+		// }
+		return true;
+	}
+
+	/
+	public function mwb_wsfw_user_wallet_balance( $request ) {
+		require_once WALLET_SYSTEM_FOR_WOOCOMMERCE_DIR_PATH . 'package/rest-api/version1/class-wallet-system-for-woocommerce-api-process.php';
+		$mwb_wsfw_api_obj = new Wallet_System_For_Woocommerce_Api_Process();
+		$parameters = $request->get_params();
+		$mwb_wsfw_resultsdata = $mwb_wsfw_api_obj->get_wallet_balance( $parameters['id'] );
+		if ( is_array( $mwb_wsfw_resultsdata ) && isset( $mwb_wsfw_resultsdata['status'] ) && 200 == $mwb_wsfw_resultsdata['status'] ) {
+			unset( $mwb_wsfw_resultsdata['status'] );
+			$mwb_wsfw_response = new WP_REST_Response( $mwb_wsfw_resultsdata['data'], 200 );
+		} else {
+			$mwb_wsfw_response = new WP_Error( $mwb_wsfw_resultsdata );
+		}
+		return $mwb_wsfw_response;
+	}
+
+	public function mwb_wsfw_edit_wallet_balance( $request ) {
+		require_once WALLET_SYSTEM_FOR_WOOCOMMERCE_DIR_PATH . 'package/rest-api/version1/class-wallet-system-for-woocommerce-api-process.php';
+		$mwb_wsfw_api_obj = new Wallet_System_For_Woocommerce_Api_Process();
+		$parameters = $request->get_params();
+		if ( isset( $parameters['amount'] ) && ! empty( $parameters['amount'] ) ) { 
+			$mwb_wsfw_resultsdata = $mwb_wsfw_api_obj->update_wallet_balance( $parameters );
+			if ( is_array( $mwb_wsfw_resultsdata ) && isset( $mwb_wsfw_resultsdata['status'] ) && 200 == $mwb_wsfw_resultsdata['status'] ) {
+				unset( $mwb_wsfw_resultsdata['status'] );
+				$mwb_wsfw_response = new WP_REST_Response( $mwb_wsfw_resultsdata['data'], 200 );
+			} else {
+				$mwb_wsfw_response = new WP_Error( $mwb_wsfw_resultsdata );
+			}
+		} else {
+			$mwb_wsfw_response = new WP_REST_Response( array( 'response' => 'Amount should be greater than 0' ), 401 );
+		}
+		return $mwb_wsfw_response;
+	}
+
+	public function mwb_wsfw_user_wallet_transactions( $request ) {
+		require_once WALLET_SYSTEM_FOR_WOOCOMMERCE_DIR_PATH . 'package/rest-api/version1/class-wallet-system-for-woocommerce-api-process.php';
+		$mwb_wsfw_api_obj = new Wallet_System_For_Woocommerce_Api_Process();
+		$parameters = $request->get_params();
+		$mwb_wsfw_resultsdata = $mwb_wsfw_api_obj->get_user_wallet_transactions( $parameters['id'] );
+		if ( is_array( $mwb_wsfw_resultsdata ) && isset( $mwb_wsfw_resultsdata['status'] ) && 200 == $mwb_wsfw_resultsdata['status'] ) {
+			unset( $mwb_wsfw_resultsdata['status'] );
+			$mwb_wsfw_response = new WP_REST_Response( $mwb_wsfw_resultsdata['data'], 200 );
+		} else {
+			$mwb_wsfw_response = new WP_Error( $mwb_wsfw_resultsdata );
+		} 
+
+		return $mwb_wsfw_response;
+	}
+	 
+}
---
+++ .history/package/rest-api/version1/class-wallet-system-for-woocommerce-api-process_20210427100838.php
@@ -0,0 +1,165 @@
+<?php
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://makewebbetter.com/
+ * @since      1.0.0
+ *
+ * @package    Wallet_System_For_Woocommerce
+ * @subpackage Wallet_System_For_Woocommerce/includes
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+if ( ! class_exists( 'Wallet_System_For_Woocommerce_Api_Process' ) ) {
+
+	/**
+	 * The plugin API class.
+	 *
+	 * This is used to define the functions and data manipulation for custom endpoints.
+	 *
+	 * @since      1.0.0
+	 * @package    Hydroshop_Api_Management
+	 * @subpackage Hydroshop_Api_Management/includes
+	 * @author     MakeWebBetter <makewebbetter.com>
+	 */
+	class Wallet_System_For_Woocommerce_Api_Process {
+
+		/**
+		 * Initialize the class and set its properties.
+		 *
+		 * @since    1.0.0
+		 */
+		public function __construct() {
+
+		}
+
+		/**
+		 * Define the function to process data for custom endpoint.
+		 *
+		 * @since    1.0.0
+		 * @param   Array $wsfw_request  data of requesting headers and other information.
+		 * @return  Array $mwb_wsfw_rest_response    returns processed data and status of operations.
+		 */
+		public function mwb_wsfw_default_process( $wsfw_request ) {
+			$mwb_wsfw_rest_response = array();
+
+			// Write your custom code here.
+
+			$mwb_wsfw_rest_response['status'] = 200;
+			//$mwb_wsfw_rest_response['data'] = $wsfw_request->get_headers();
+			$mwb_wsfw_rest_response['data'] = 'hello';
+			return $mwb_wsfw_rest_response;
+		}
+
+		/**
+		 * REturn user's wallet balance
+		 *
+		 * @param int $user_id
+		 * @return array
+		 */
+		public function get_wallet_balance( $user_id ) {
+			$mwb_wsfw_rest_response = array();
+
+			if ( ! empty( $user_id ) ) {
+				$user = get_user_by( 'id', $user_id );
+				if ( $user ) {
+					$wallet_bal = get_user_meta( $user_id, 'mwb_wallet', true );
+					if ( empty( $wallet_bal ) ) {
+						$wallet_bal = "0.00";
+					}
+					$mwb_wsfw_rest_response['data'] = $wallet_bal;
+				} else {
+					$mwb_wsfw_rest_response['data'] = esc_html__( 'User does not exist', 'wallet-system-for-woocommerce' );
+				}
+            }
+			$mwb_wsfw_rest_response['status'] = 200;
+		
+			return $mwb_wsfw_rest_response;
+		}
+
+		/**
+		 * REturn user's wallet balance
+		 *
+		 * @param int $user_id
+		 * @return array
+		 */
+		public function update_wallet_balance( $request ) {
+			$mwb_wsfw_rest_response = array();
+
+			$user_id = $request['id'];
+			if ( ! empty( $user_id ) ) {
+				$user = get_user_by( 'id', $user_id );
+				if ( $user ) {
+					$wallet_payment_gateway = new Wallet_System_For_Woocommerce();
+					$updated_amount = sanitize_text_field( $request['amount'] );
+					$wallet_action  = sanitize_text_field( $request['transaction_type'] );
+					$payment_method = sanitize_text_field( $request['payment_method'] );
+					$note           = ! empty( $request['note'] )? sanitize_text_field( $request['note'] ) : '';
+					$order_id       = ! empty( $request['order_id'] )? sanitize_text_field( $request['order_id'] ) : '';
+					$wallet         = get_user_meta( $user_id, 'mwb_wallet', true );
+
+					if ( 'credit' == $wallet_action ) { 
+						$wallet += $updated_amount;
+						$update_wallet = update_user_meta( $user_id, 'mwb_wallet', $wallet );
+						$transaction_type = 'Credited by admin';
+						$mail_message     = __( 'Merchant has credited your wallet by '. wc_price( $updated_amount ), 'wallet-system-for-woocommerce' );
+
+					} elseif ( 'debit' == $wallet_action ) { 
+						if ( $wallet < $updated_amount ) {
+							$wallet = 0;
+						} else {
+							$wallet -= $updated_amount;
+						}
+						$update_wallet = update_user_meta( $user_id, 'mwb_wallet', abs($wallet) );
+						$transaction_type = 'Debited by admin';
+						$mail_message     = __( 'Merchant has deducted '. wc_price( $updated_amount ). ' from your wallet.', 'wallet-system-for-woocommerce' );
+						
+					}
+					if ( $update_wallet ) {
+						$wallet         = get_user_meta( $user_id, 'mwb_wallet', true );
+						
+					}
+					$data = array( 'response' => 'success', 'balance' => $wallet, 'transaction_id' => '' );
+					$mwb_wsfw_rest_response['data'] = $data;
+				} else {
+					$mwb_wsfw_rest_response['data'] = esc_html__( 'User does not exist', 'wallet-system-for-woocommerce' );
+				}
+            }
+			$mwb_wsfw_rest_response['status'] = 200;
+		
+			return $mwb_wsfw_rest_response;
+		}
+
+		/**
+		 * Return user's wallet balance
+		 *
+		 * @param int $user_id
+		 * @return array
+		 */
+		public function get_user_wallet_transactions( $user_id ) {
+			$mwb_wsfw_rest_response = array();
+
+			if ( ! empty( $user_id ) ) {
+				$user = get_user_by( 'id', $user_id );
+				if ( $user ) {
+					global $wpdb;
+					$table_name = $wpdb->prefix . 'mwb_wsfw_wallet_transaction';
+					$transactions = $wpdb->get_results( "SELECT * FROM $table_name WHERE user_id = $user_id ORDER BY Id" );
+					if ( ! empty( $transactions ) && is_array($transactions ) ) {
+						$mwb_wsfw_rest_response['data'] = $transactions;
+					} else {
+						$mwb_wsfw_rest_response['data'] = array();
+					}
+				} else {
+					$mwb_wsfw_rest_response['data'] = esc_html__( 'User does not exist', 'wallet-system-for-woocommerce' );
+				}
+            }
+			$mwb_wsfw_rest_response['status'] = 200;
+		
+			return $mwb_wsfw_rest_response;
+		}
+
+	}
+}
