ff4c87b6d74060b9acb1cb53f55bea887ad0fd2e
shobhapatwal-cedcoss <shobhapatwal@makewebbetter.com>
2021-05-28 11:31:07 +0530

    fixed js issue

--- public/src/js/wallet-system-for-woocommerce-public.js
+++ public/src/js/wallet-system-for-woocommerce-public.js
@@ -36,8 +36,9 @@
 
 				$( '#partial_payment_wallet' ).on( 'click', function() {
 					if ( $('#partial_payment_wallet:checked').val() == 'enable' ) {
-						$( '.partial_payment' ).after('<tr class="partial_amount" ><td colspan="2"><p class="ajax_msg"></p><div class="discount_box"><p class="wallet-amount">Amount want to use from wallet</p><p class="wallet-amount form-row form-row-first"><input type="number" class="input-text" name="wallet_amount" id="wallet_amount" placeholder="Amount want to use from wallet"></p><p class="form-row form-row-last"><button type="button" class="button" id="apply_wallet" name="apply_wallet" value="Apply coupon">Apply wallet</button></p></div></td></tr>');
-						
+						if ($('.partial_amount').length === 0) {
+							$( '.partial_payment' ).after('<tr class="partial_amount" ><td colspan="2"><p class="ajax_msg"></p><div class="discount_box"><p class="wallet-amount">Amount want to use from wallet</p><p class="wallet-amount form-row form-row-first"><input type="number" class="input-text" name="wallet_amount" id="wallet_amount" placeholder="Amount want to use from wallet"></p><p class="form-row form-row-last"><button type="button" class="button" id="apply_wallet" name="apply_wallet" value="Apply coupon">Apply wallet</button></p></div></td></tr>');
+						}
 					} else {
 						$( '.partial_amount' ).remove();
 						$( '.woocommerce-checkout-review-order-table .fee' ).remove();
