3a0e5a0724ad45c1619e5d4e7d6e5a2757796186
shobhapatwal-cedcoss <shobhapatwal@makewebbetter.com>
2021-06-12 12:05:41 +0530

    updated readme file

--- wallet-system-for-woocommerce.php
+++ wallet-system-for-woocommerce.php
@@ -24,7 +24,7 @@
  * Requires at least: 4.6
  * Tested up to:      5.7.2
  * WC requires at least: 3.0.0
- * WC tested up to:      5.2.0
+ * WC tested up to:      5.4.1
  *
  * License:           GNU General Public License v3.0
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.html
