52ac67ff816962d0510200a2e117565fb4ae22a7
shobhapatwal-cedcoss <shobhapatwal@makewebbetter.com>
2021-08-02 17:06:42 +0530

    updated versions in main file

--- wallet-system-for-woocommerce.php
+++ wallet-system-for-woocommerce.php
@@ -22,9 +22,9 @@
  * Domain Path:       /languages
  *
  * Requires at least: 4.6
- * Tested up to:      5.7.2
+ * Tested up to:      5.8
  * WC requires at least: 3.0.0
- * WC tested up to:      5.4.1
+ * WC tested up to:      5.5.2
  *
  * License:           GNU General Public License v3.0
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.html
