bbc31863119e95968ffe4b4d46066b7b3785f496
ananya-cedcoss <ananyashukla@makewebbetter.com>
2023-06-02 12:47:59 +0530

    update

--- wallet-system-for-woocommerce.php
+++ wallet-system-for-woocommerce.php
@@ -24,7 +24,7 @@
  * WC Requires at least: 5.5.0
  * WC tested up to: 7.7.2
  * WP Requires at least: 5.5.0
- * WP tested up to: 6.2.0
+ * WP tested up to: 6.2.2
  * Requires PHP: 7.3.5 or Higher
  *
  * License:           GNU General Public License v3.0
