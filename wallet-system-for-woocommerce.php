51ebc0241634f3d6dc9d3941f0b45fa232ee4c0f
ananya-cedcoss <ananyashukla@makewebbetter.com>
2023-11-03 17:35:01 +0530

    readme update

--- wallet-system-for-woocommerce.php
+++ wallet-system-for-woocommerce.php
@@ -22,7 +22,7 @@
  * Domain Path:       /languages
  *
  * WC Requires at least: 5.5.0
- * WC tested up to: 8.2.0
+ * WC tested up to: 8.2.1
  * WP Requires at least: 5.5.0
  * WP tested up to: 6.3.2
  * Requires PHP: 7.3.5 or Higher
