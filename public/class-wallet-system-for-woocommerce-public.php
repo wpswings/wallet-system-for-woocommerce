99cd6da7fe049e18cdb5816864887b0bbc0bfe0c
ananya-cedcoss <ananyashukla@makewebbetter.com>
2024-02-22 18:43:19 +0530

    readme updated

--- public/class-wallet-system-for-woocommerce-public.php
+++ public/class-wallet-system-for-woocommerce-public.php
@@ -2371,21 +2371,6 @@ class Wallet_System_For_Woocommerce_Public {
 			}
 		}
 
-		$userid                 = $order->get_user_id();
-		$walletamount           = get_user_meta( $userid, 'wps_wallet', true );
-
-		if ( intval( $walletamount ) < intval( $fee_total ) ) {
-			$toral = $order->get_total();
-			$order->set_total( $toral + $fee_total );
-			$order->save();
-			// WC()->session->__unset( 'is_wallet_partial_payment_cart_total_tax' );
-			// WC()->session->__unset( 'is_wallet_partial_payment_block' );
-			// WC()->session->__unset( 'is_wallet_partial_payment_cart_total_value' );
-			return;
-		}
-
-
-
 		if ( ! empty( $fee_total_tax ) ) {
 			$order_id = $order->get_id();
 			$order_tax = '';
