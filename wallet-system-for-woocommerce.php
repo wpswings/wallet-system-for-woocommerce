a41d3803fa3b2ef2d10466487c135df7320997d7
Shivamddeveloper <shivamsrivastava@makewebbetter.com>
2024-09-30 10:11:12 +0530

    language update

--- wallet-system-for-woocommerce.php
+++ wallet-system-for-woocommerce.php
@@ -21,8 +21,8 @@
  * Text Domain:       wallet-system-for-woocommerce
  * Domain Path:       /languages
  * Requires Plugins: woocommerce
- * WC Requires at least: 6.5.2
- * WC tested up to: 9.2.3
+ * WC Requires at least: 6.6.2
+ * WC tested up to: 9.3.3
  * WP Requires at least: 5.5.0
  * WP tested up to: 6.6.1
  * Requires PHP: 7.2.24
