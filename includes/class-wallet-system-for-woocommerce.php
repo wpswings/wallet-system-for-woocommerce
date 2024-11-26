71d5d4cdbe410fba351a87c75889a1a05dc1baca
Shivamddeveloper <shivamsrivastava@makewebbetter.com>
2024-11-26 10:48:05 +0530

    version 2.6.1 with latest wp and wc

--- includes/class-wallet-system-for-woocommerce.php
+++ includes/class-wallet-system-for-woocommerce.php
@@ -81,7 +81,7 @@ class Wallet_System_For_Woocommerce {
 			$this->version = WALLET_SYSTEM_FOR_WOOCOMMERCE_VERSION;
 		} else {
 
-			$this->version = '2.6.0';
+			$this->version = '2.6.1';
 		}
 
 		$this->plugin_name = 'wallet-system-for-woocommerce';
